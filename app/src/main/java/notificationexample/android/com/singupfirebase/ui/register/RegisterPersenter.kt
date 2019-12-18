8308ecf31f00268e07825359092cff330760afb5
suphawan <suphawanbow97@gmail.com>
2019-12-18 14:16:34 +0700

    (bow) register

--- app/src/main/java/notificationexample/android/com/singupfirebase/ui/register/RegisterPersenter.kt
+++ app/src/main/java/notificationexample/android/com/singupfirebase/ui/register/RegisterPersenter.kt
@@ -45,7 +45,6 @@ class RegisterPersenter(private val view: RegisterView.View) {
                             .addOnCompleteListener(object : OnCompleteListener<Void> {
                                 override fun onComplete(p0: Task<Void>) {
                                     view.onSuccess(p0.isSuccessful)
-//                                    dsdfghdtyui
 //
                                 }
 
